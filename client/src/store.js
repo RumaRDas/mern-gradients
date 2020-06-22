e748fa95b583add2ebb329be03ca606e7d109954
Subroto   Biswas <37511552+toofaniCoder@users.noreply.github.com>
2020-06-22 21:32:14 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -8,14 +8,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const fbConfig = {
-  apiKey: "AIzaSyD0ff0B8c4VFVHIkt-TjqBXuiX12zl50kA",
-  authDomain: "mern-gradient.firebaseapp.com",
-  databaseURL: "https://mern-gradient.firebaseio.com",
-  projectId: "mern-gradient",
-  storageBucket: "mern-gradient.appspot.com",
-  messagingSenderId: "813055936676",
-  appId: "1:813055936676:web:257b9a9af10f7c9b4a3e31",
-  measurementId: "G-K28LLFKJ35",
+ YOUR_FIREBASE_CONFIG_HERE
 };
 
 firebase.initializeApp(fbConfig);
